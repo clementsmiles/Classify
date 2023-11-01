660ee3e5b6f2bd621e88911dc6cd3d1234188425
clementsmiles <149609966+clementsmiles@users.noreply.github.com>
2023-11-01 13:27:06 -0400

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,10 +1,16 @@
 package source.Classify;
+import java.util.ArrayList;
 
 public class Professor {
     
-    public String name;
+    // Name of professor
+    private String name;
+    // ArrayList of all courses professor is planning on teaching.
+    private ArrayList<Course> courses;
     
-    public Professor(String name) {
+    // Constructor for a Professor object.
+    public Professor(String name, ArrayList<Course> courses) {
         this.name = name;
+        this.courses = courses;
     }
 }
