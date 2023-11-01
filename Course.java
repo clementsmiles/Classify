b8e0f03b533f5f17b747edbe40dad60164751ad6
clementsmiles <149609966+clementsmiles@users.noreply.github.com>
2023-11-01 13:08:53 -0400

    Added Professor

--- Course.java
+++ Course.java
@@ -1,5 +1,17 @@
 package source.Classify;
 
 public class Course {
-
+    
+    private String department;
+    private Professor professor;
+    private Course[] prerequisites;
+    
+    
+    // Constructor for Course object.
+    public Course(String department, Professor professor) {
+        this.department = department;
+        this.professor = professor;
+    }
+    
+    
 }
---
+++ Professor.java
@@ -0,0 +1,10 @@
+package source.Classify;
+
+public class Professor {
+    
+    public String name;
+    
+    public Professor(String name) {
+        this.name = name;
+    }
+}
