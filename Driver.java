cbdfef79be77719e645ed12e9b6aa02de13915ee
clementsmiles <149609966+clementsmiles@users.noreply.github.com>
2023-11-02 12:25:57 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -7,6 +7,26 @@ package source.Classify;
 
 public class Driver {
     
+    /**
+     * This method should allow the user to input a course or professor
+     * (could potentially be an overloaded method) and show the results
+     * for their search query. This menu should also allow the user to
+     * go back to the previous menu, and select between course or professor.
+     * Copyright 2023 Insert Names Here
+     */
+    public static void searchMenu() {
+        
+    }
+    
+    /**
+     * This method should show the first menu allowing the user
+     * to select the mode they'd like to search in. (Classes/Professor).
+     * Copyright 2023 Insert Names Here
+     */
+    public static void modeMenu() {
+        
+    }
+    
     public static void main(String[] args) {
         
     }
