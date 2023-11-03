82cadbc4cc5b14ddb4391d375e8c6e9e54553e5b
clementsmiles <149609966+clementsmiles@users.noreply.github.com>
2023-11-03 12:45:16 -0400

    Update Course.java

--- Course.java
+++ Course.java
@@ -7,12 +7,9 @@ package source.Classify;
 
 public class Course {
     
-    // Identifier for the department the class is hosted by.
     private String department;
-    // Professor teaching this class.
     private Professor professor;
     
-    // Constructor for Course object.
     public Course(String department, Professor professor) {
         this.department = department;
         this.professor = professor;
