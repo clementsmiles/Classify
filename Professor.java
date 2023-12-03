e5ad8f74565ec2902a464887eefd8109eeb86dea
samuelhpclark <149610340+samuelhpclark@users.noreply.github.com>
2023-12-03 17:46:20 -0500

    Update Professor.java
    Added rateMyProfessor() method

--- Professor.java
+++ Professor.java
@@ -1,48 +1,105 @@
 /**
  * Professor Object Class
- * Copyright 2023 Miles Clements
+ * Copyright 2023 Miles Clements, Sam Clark
  */
 
 package source;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+
 
 public class Professor {
     
     private String name;
+    private String rating;
+    private String[] comments;
     private ArrayList<Course> courses;
     
     public Professor(String name, ArrayList<Course> courses) {
         this.name = name;
         this.courses = courses;
+        rateMyProfessor();
     }
     
     public String getName() {
         return this.name;
     }
     
-    public String getInfo() {
-        String s = "";
-        for(int i = 0; i < courses.size(); i++) {
-            s += (courses.get(i).getDepartment() + " " + courses.get(i).getCourseNum() + 
-                     " " + courses.get(i).getDays() + " " + courses.get(i).getTime() + 
-                     " " + courses.get(i).getCRN() + " " +courses.get(i).getProfName() + "\n");
-            
-        }
-        return s;
-    }
-    
     public void addCourse(Course c) {
         courses.add(c);
     }
     
-    public String printCourse() {
+    public void printCourse() {
         
     for(int i = 0; i < courses.size(); i++) {
-        
-       return this.courses.get(i).profDataPrint();
+       this.courses.get(i).profDataPrint();
     }
-    return name;
     
         
     }
+
+    public void rateMyProfessor() throws IOException {
+        Scanner fileReader = new Scanner(new File("rateMyProfID.csv"));
+		String id = "";
+		fileReader.useDelimiter(",");
+		
+		while(fileReader.hasNext()) {
+			String content = fileReader.next();
+			if (content.indexOf(this.name) != -1) {
+				id = fileReader.nextLine();
+				id = id.substring(1, id.length());
+			}
+		}
+        if (id.equals("")) {
+            this.rating = "NA";
+        } else {
+            
+		    String url = "https://www.ratemyprofessors.com/professor/"
+                    + profToRating.get(this.name);
+		    URL obj = new URL(url);
+		    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		    BufferedReader in = new BufferedReader
+				    (new InputStreamReader(con.getInputStream()));
+		    String inputLine;
+		    StringBuilder response = new StringBuilder();
+		    while ((inputLine = in.readLine()) != null) {
+			    response.append(inputLine);
+		    }
+		    in.close();
+		    String html = response.toString();
+		    Document doc = Jsoup.parse(html);
+
+		    String rating = "NA";
+		    Elements deez = doc.select("div.RatingValue__Numerator-qw8sqy-2");
+            String rating = "NA";
+		    for (Element dis : deez) {
+			    attribute = dis.html();
+		    }
+		    this.rating = rating;
+            
+		    Elements deez2 = doc.select("div.Comments__StyledComments-dzzyvm-0");
+            String[] comments = new String[deez2.size()];
+            int index = 0;
+		    for (Element dis : deez2) {
+			    comments[i] = dis;
+                index++;
+		    }
+
+            
+        }
+
+	}
+    
 }
