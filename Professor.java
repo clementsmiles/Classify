5bc6db4806f3d84480e4c991bd6c28514cbd51fa
samuelhpclark <149610340+samuelhpclark@users.noreply.github.com>
2023-12-03 19:37:51 -0500

    Update Professor.java
    Fixed problems

--- Professor.java
+++ Professor.java
@@ -5,19 +5,18 @@
 
 package source;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 
 
 public class Professor {
@@ -52,50 +51,52 @@ public class Professor {
 
     public void rateMyProfessor() throws IOException {
         Scanner fileReader = new Scanner(new File("rateMyProfID.csv"));
-		String id = "";
-		fileReader.useDelimiter(",");
+	String id = "";
+	fileReader.useDelimiter(",");
 		
-		while(fileReader.hasNext()) {
-			String content = fileReader.next();
-			if (content.indexOf(this.name) != -1) {
-				id = fileReader.nextLine();
-				id = id.substring(1, id.length());
-			}
+	while(fileReader.hasNext()) {
+		String content = fileReader.next();
+		if (content.indexOf(this.name) != -1) {
+			id = fileReader.nextLine();
+			id = id.substring(1, id.length());
 		}
+	}
         if (id.equals("")) {
             this.rating = "NA";
+	    this.comments = new String[1];
+	    this.comments[0] = "NA";
         } else {
             
-		    String url = "https://www.ratemyprofessors.com/professor/"
-                    + profToRating.get(this.name);
-		    URL obj = new URL(url);
-		    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		    BufferedReader in = new BufferedReader
-				    (new InputStreamReader(con.getInputStream()));
-		    String inputLine;
-		    StringBuilder response = new StringBuilder();
-		    while ((inputLine = in.readLine()) != null) {
-			    response.append(inputLine);
-		    }
-		    in.close();
-		    String html = response.toString();
-		    Document doc = Jsoup.parse(html);
+	String url = "https://www.ratemyprofessors.com/professor/"
+                    + id;
+	URL obj = new URL(url);
+	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	BufferedReader in = new BufferedReader
+			(new InputStreamReader(con.getInputStream()));
+	String inputLine;
+	StringBuilder response = new StringBuilder();
+	while ((inputLine = in.readLine()) != null) {
+		response.append(inputLine);
+	}
+	in.close();
+	String html = response.toString();
+	Document doc = Jsoup.parse(html);
 
-		    String rating = "NA";
-		    Elements deez = doc.select("div.RatingValue__Numerator-qw8sqy-2");
-            String rating = "NA";
-		    for (Element dis : deez) {
-			    attribute = dis.html();
-		    }
-		    this.rating = rating;
+	
+	Elements deez = doc.select("div.RatingValue__Numerator-qw8sqy-2");
+        String rating = "NA";
+	for (Element dis : deez) {
+		rating = dis.html();
+	}
+	this.rating = rating;
             
-		    Elements deez2 = doc.select("div.Comments__StyledComments-dzzyvm-0");
-            String[] comments = new String[deez2.size()];
-            int index = 0;
-		    for (Element dis : deez2) {
-			    comments[i] = dis;
-                index++;
-		    }
+	Elements deez2 = doc.select("div.Comments__StyledComments-dzzyvm-0");
+        String[] comments = new String[deez2.size()];
+        int i = 0;
+	for (Element dis : deez2) {
+		comments[i] = dis.html();
+        	i++;
+	}
 
             
         }
